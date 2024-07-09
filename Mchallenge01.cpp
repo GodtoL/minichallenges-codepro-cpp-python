eb7d1bbfab021237553867d24d7251ed7731ef1e
GodtoL <eliasvera793@gmail.com>
2024-07-09 15:11:19 -0400

    Mchallenge01.cpp

--- Mchallenge01.cpp
+++ Mchallenge01.cpp
@@ -2,34 +2,31 @@
 #include <vector>
 #include <Windows.h>
 
-bool busquedaBinaria(const std::vector<int>& lista, int numero) {
-    int inicio = 0;
-    int final = lista.size() - 1;
-
-    while (inicio <= final) {
-        int medio = inicio + (final - inicio) / 2; // Esto ayuda a evitar posibles desbordamientos
-
-        if (lista[medio] == numero) {
-            return true;
-        } else if (lista[medio] < numero) {
-            inicio = medio + 1;
-        } else {
-            final = medio - 1;
-        }
+bool binarySearch(std::vector<int>& list , int number){
+    int start = 0;
+    int end = list.size() -1;
+    
+    while(start <= end ){
+    int mid = start + (start - end) / 2;
+    if (list[mid] == number){
+        return true;
+    }else if (list[mid] < number){
+        start = mid + 1;}
+    else{
+        end = mid- 1;}
     }
-
     return false;
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    std::vector<int> listaOrdenada = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
-    int numeroABuscar;
+    std::vector<int> ordeneredList = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+    int numberTarget;
     std::cout << "Ingrese el número que desea buscar: ";
-    std::cin >> numeroABuscar;
+    std::cin >> numberTarget;
 
-    bool resultado = busquedaBinaria(listaOrdenada, numeroABuscar);
-    std::cout << "El número " << numeroABuscar << " está en la lista: " << (resultado ? "Sí" : "No") << std::endl;
+    bool result = binarySearch(ordeneredList, numberTarget);
+    std::cout << "El número " << numberTarget << " está en la lista: " << (result ? "Sí" : "No") << std::endl;
 
     return 0;
 }
